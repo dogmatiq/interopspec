c034cee26bdc57cb2bcd1b77f99fad2806857927
Danil Petrov <danilpetrovvi@gmail.com>
2020-12-14 09:54:12 +1000

    Rename test desc to match the new method name.
    Co-authored-by: James Harris <mailjamesharris@gmail.com>

--- envelopespec/envelope_test.go
+++ envelopespec/envelope_test.go
@@ -8,7 +8,8 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("func CheckWellFormed()", func() {
+var _ = Describe("type Envelope", func() {
+	Describe("func Validate()", func() {
 	var env *Envelope
 
 	BeforeEach(func() {

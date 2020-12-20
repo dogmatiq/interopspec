9ef6fe6e48ba0c89dbf3ad5f5eebb8ed6251ce8b
James Harris <mailjamesharris@gmail.com>
2020-12-20 12:57:03 +1000

    Rename presence API to match new conventions.

--- presencespec/presenceapi.pb.go
+++ presencespec/presenceapi.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.25.0
 // 	protoc        v3.14.0
-// source: github.com/dogmatiq/interopspec/presencespec/api.proto
+// source: github.com/dogmatiq/interopspec/presencespec/presenceapi.proto
 
 package presencespec
 
@@ -38,7 +38,7 @@ type WatchRequest struct {
 func (x *WatchRequest) Reset() {
 	*x = WatchRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_github_com_dogmatiq_interopspec_presencespec_api_proto_msgTypes[0]
+		mi := &file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -51,7 +51,7 @@ func (x *WatchRequest) String() string {
 func (*WatchRequest) ProtoMessage() {}
 
 func (x *WatchRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_github_com_dogmatiq_interopspec_presencespec_api_proto_msgTypes[0]
+	mi := &file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -64,7 +64,7 @@ func (x *WatchRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use WatchRequest.ProtoReflect.Descriptor instead.
 func (*WatchRequest) Descriptor() ([]byte, []int) {
-	return file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDescGZIP(), []int{0}
+	return file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDescGZIP(), []int{0}
 }
 
 type WatchResponse struct {
@@ -76,7 +76,7 @@ type WatchResponse struct {
 func (x *WatchResponse) Reset() {
 	*x = WatchResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_github_com_dogmatiq_interopspec_presencespec_api_proto_msgTypes[1]
+		mi := &file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -89,7 +89,7 @@ func (x *WatchResponse) String() string {
 func (*WatchResponse) ProtoMessage() {}
 
 func (x *WatchResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_github_com_dogmatiq_interopspec_presencespec_api_proto_msgTypes[1]
+	mi := &file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -102,20 +102,21 @@ func (x *WatchResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use WatchResponse.ProtoReflect.Descriptor instead.
 func (*WatchResponse) Descriptor() ([]byte, []int) {
-	return file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDescGZIP(), []int{1}
+	return file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDescGZIP(), []int{1}
 }
 
-var File_github_com_dogmatiq_interopspec_presencespec_api_proto protoreflect.FileDescriptor
+var File_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto protoreflect.FileDescriptor
 
-var file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDesc = []byte{
-	0x0a, 0x36, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x64, 0x6f, 0x67,
+var file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDesc = []byte{
+	0x0a, 0x3e, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x64, 0x6f, 0x67,
 	0x6d, 0x61, 0x74, 0x69, 0x71, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6f, 0x70, 0x73, 0x70, 0x65,
-	0x63, 0x2f, 0x70, 0x72, 0x65, 0x73, 0x65, 0x6e, 0x63, 0x65, 0x73, 0x70, 0x65, 0x63, 0x2f, 0x61,
-	0x70, 0x69, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x19, 0x64, 0x6f, 0x67, 0x6d, 0x61, 0x2e,
-	0x69, 0x6e, 0x74, 0x65, 0x72, 0x6f, 0x70, 0x2e, 0x76, 0x31, 0x2e, 0x70, 0x72, 0x65, 0x73, 0x65,
-	0x6e, 0x63, 0x65, 0x22, 0x0e, 0x0a, 0x0c, 0x57, 0x61, 0x74, 0x63, 0x68, 0x52, 0x65, 0x71, 0x75,
-	0x65, 0x73, 0x74, 0x22, 0x0f, 0x0a, 0x0d, 0x57, 0x61, 0x74, 0x63, 0x68, 0x52, 0x65, 0x73, 0x70,
-	0x6f, 0x6e, 0x73, 0x65, 0x32, 0x63, 0x0a, 0x03, 0x41, 0x50, 0x49, 0x12, 0x5c, 0x0a, 0x05, 0x57,
+	0x63, 0x2f, 0x70, 0x72, 0x65, 0x73, 0x65, 0x6e, 0x63, 0x65, 0x73, 0x70, 0x65, 0x63, 0x2f, 0x70,
+	0x72, 0x65, 0x73, 0x65, 0x6e, 0x63, 0x65, 0x61, 0x70, 0x69, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x12, 0x19, 0x64, 0x6f, 0x67, 0x6d, 0x61, 0x2e, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6f, 0x70, 0x2e,
+	0x76, 0x31, 0x2e, 0x70, 0x72, 0x65, 0x73, 0x65, 0x6e, 0x63, 0x65, 0x22, 0x0e, 0x0a, 0x0c, 0x57,
+	0x61, 0x74, 0x63, 0x68, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x22, 0x0f, 0x0a, 0x0d, 0x57,
+	0x61, 0x74, 0x63, 0x68, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x32, 0x6b, 0x0a, 0x0b,
+	0x50, 0x72, 0x65, 0x73, 0x65, 0x6e, 0x63, 0x65, 0x41, 0x50, 0x49, 0x12, 0x5c, 0x0a, 0x05, 0x57,
 	0x61, 0x74, 0x63, 0x68, 0x12, 0x27, 0x2e, 0x64, 0x6f, 0x67, 0x6d, 0x61, 0x2e, 0x69, 0x6e, 0x74,
 	0x65, 0x72, 0x6f, 0x70, 0x2e, 0x76, 0x31, 0x2e, 0x70, 0x72, 0x65, 0x73, 0x65, 0x6e, 0x63, 0x65,
 	0x2e, 0x57, 0x61, 0x74, 0x63, 0x68, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x28, 0x2e,
@@ -129,25 +130,25 @@ var file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDesc = []byte
 }
 
 var (
-	file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDescOnce sync.Once
-	file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDescData = file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDesc
+	file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDescOnce sync.Once
+	file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDescData = file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDesc
 )
 
-func file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDescGZIP() []byte {
-	file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDescOnce.Do(func() {
-		file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDescData = protoimpl.X.CompressGZIP(file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDescData)
+func file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDescGZIP() []byte {
+	file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDescOnce.Do(func() {
+		file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDescData = protoimpl.X.CompressGZIP(file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDescData)
 	})
-	return file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDescData
+	return file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDescData
 }
 
-var file_github_com_dogmatiq_interopspec_presencespec_api_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_github_com_dogmatiq_interopspec_presencespec_api_proto_goTypes = []interface{}{
+var file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_goTypes = []interface{}{
 	(*WatchRequest)(nil),  // 0: dogma.interop.v1.presence.WatchRequest
 	(*WatchResponse)(nil), // 1: dogma.interop.v1.presence.WatchResponse
 }
-var file_github_com_dogmatiq_interopspec_presencespec_api_proto_depIdxs = []int32{
-	0, // 0: dogma.interop.v1.presence.API.Watch:input_type -> dogma.interop.v1.presence.WatchRequest
-	1, // 1: dogma.interop.v1.presence.API.Watch:output_type -> dogma.interop.v1.presence.WatchResponse
+var file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_depIdxs = []int32{
+	0, // 0: dogma.interop.v1.presence.PresenceAPI.Watch:input_type -> dogma.interop.v1.presence.WatchRequest
+	1, // 1: dogma.interop.v1.presence.PresenceAPI.Watch:output_type -> dogma.interop.v1.presence.WatchResponse
 	1, // [1:2] is the sub-list for method output_type
 	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -155,13 +156,13 @@ var file_github_com_dogmatiq_interopspec_presencespec_api_proto_depIdxs = []int3
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_github_com_dogmatiq_interopspec_presencespec_api_proto_init() }
-func file_github_com_dogmatiq_interopspec_presencespec_api_proto_init() {
-	if File_github_com_dogmatiq_interopspec_presencespec_api_proto != nil {
+func init() { file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_init() }
+func file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_init() {
+	if File_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_github_com_dogmatiq_interopspec_presencespec_api_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*WatchRequest); i {
 			case 0:
 				return &v.state
@@ -173,7 +174,7 @@ func file_github_com_dogmatiq_interopspec_presencespec_api_proto_init() {
 				return nil
 			}
 		}
-		file_github_com_dogmatiq_interopspec_presencespec_api_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*WatchResponse); i {
 			case 0:
 				return &v.state
@@ -190,20 +191,20 @@ func file_github_com_dogmatiq_interopspec_presencespec_api_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDesc,
+			RawDescriptor: file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_github_com_dogmatiq_interopspec_presencespec_api_proto_goTypes,
-		DependencyIndexes: file_github_com_dogmatiq_interopspec_presencespec_api_proto_depIdxs,
-		MessageInfos:      file_github_com_dogmatiq_interopspec_presencespec_api_proto_msgTypes,
+		GoTypes:           file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_goTypes,
+		DependencyIndexes: file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_depIdxs,
+		MessageInfos:      file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_msgTypes,
 	}.Build()
-	File_github_com_dogmatiq_interopspec_presencespec_api_proto = out.File
-	file_github_com_dogmatiq_interopspec_presencespec_api_proto_rawDesc = nil
-	file_github_com_dogmatiq_interopspec_presencespec_api_proto_goTypes = nil
-	file_github_com_dogmatiq_interopspec_presencespec_api_proto_depIdxs = nil
+	File_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto = out.File
+	file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_rawDesc = nil
+	file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_goTypes = nil
+	file_github_com_dogmatiq_interopspec_presencespec_presenceapi_proto_depIdxs = nil
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -214,33 +215,33 @@ var _ grpc.ClientConnInterface
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion6
 
-// APIClient is the client API for API service.
+// PresenceAPIClient is the client API for PresenceAPI service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type APIClient interface {
+type PresenceAPIClient interface {
 	// Watch starts watching the server until it goes offline.
 	//
 	// The server MUST NOT send any responses.
 	//
 	// The caller SHOULD await a response (which is never sent) until an error
 	// occurs, indicating that the server has gone offline.
-	Watch(ctx context.Context, in *WatchRequest, opts ...grpc.CallOption) (API_WatchClient, error)
+	Watch(ctx context.Context, in *WatchRequest, opts ...grpc.CallOption) (PresenceAPI_WatchClient, error)
 }
 
-type aPIClient struct {
+type presenceAPIClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewAPIClient(cc grpc.ClientConnInterface) APIClient {
-	return &aPIClient{cc}
+func NewPresenceAPIClient(cc grpc.ClientConnInterface) PresenceAPIClient {
+	return &presenceAPIClient{cc}
 }
 
-func (c *aPIClient) Watch(ctx context.Context, in *WatchRequest, opts ...grpc.CallOption) (API_WatchClient, error) {
-	stream, err := c.cc.NewStream(ctx, &_API_serviceDesc.Streams[0], "/dogma.interop.v1.presence.API/Watch", opts...)
+func (c *presenceAPIClient) Watch(ctx context.Context, in *WatchRequest, opts ...grpc.CallOption) (PresenceAPI_WatchClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_PresenceAPI_serviceDesc.Streams[0], "/dogma.interop.v1.presence.PresenceAPI/Watch", opts...)
 	if err != nil {
 		return nil, err
 	}
-	x := &aPIWatchClient{stream}
+	x := &presenceAPIWatchClient{stream}
 	if err := x.ClientStream.SendMsg(in); err != nil {
 		return nil, err
 	}
@@ -250,16 +251,16 @@ func (c *aPIClient) Watch(ctx context.Context, in *WatchRequest, opts ...grpc.Ca
 	return x, nil
 }
 
-type API_WatchClient interface {
+type PresenceAPI_WatchClient interface {
 	Recv() (*WatchResponse, error)
 	grpc.ClientStream
 }
 
-type aPIWatchClient struct {
+type presenceAPIWatchClient struct {
 	grpc.ClientStream
 }
 
-func (x *aPIWatchClient) Recv() (*WatchResponse, error) {
+func (x *presenceAPIWatchClient) Recv() (*WatchResponse, error) {
 	m := new(WatchResponse)
 	if err := x.ClientStream.RecvMsg(m); err != nil {
 		return nil, err
@@ -267,60 +268,60 @@ func (x *aPIWatchClient) Recv() (*WatchResponse, error) {
 	return m, nil
 }
 
-// APIServer is the server API for API service.
-type APIServer interface {
+// PresenceAPIServer is the server API for PresenceAPI service.
+type PresenceAPIServer interface {
 	// Watch starts watching the server until it goes offline.
 	//
 	// The server MUST NOT send any responses.
 	//
 	// The caller SHOULD await a response (which is never sent) until an error
 	// occurs, indicating that the server has gone offline.
-	Watch(*WatchRequest, API_WatchServer) error
+	Watch(*WatchRequest, PresenceAPI_WatchServer) error
 }
 
-// UnimplementedAPIServer can be embedded to have forward compatible implementations.
-type UnimplementedAPIServer struct {
+// UnimplementedPresenceAPIServer can be embedded to have forward compatible implementations.
+type UnimplementedPresenceAPIServer struct {
 }
 
-func (*UnimplementedAPIServer) Watch(*WatchRequest, API_WatchServer) error {
+func (*UnimplementedPresenceAPIServer) Watch(*WatchRequest, PresenceAPI_WatchServer) error {
 	return status.Errorf(codes.Unimplemented, "method Watch not implemented")
 }
 
-func RegisterAPIServer(s *grpc.Server, srv APIServer) {
-	s.RegisterService(&_API_serviceDesc, srv)
+func RegisterPresenceAPIServer(s *grpc.Server, srv PresenceAPIServer) {
+	s.RegisterService(&_PresenceAPI_serviceDesc, srv)
 }
 
-func _API_Watch_Handler(srv interface{}, stream grpc.ServerStream) error {
+func _PresenceAPI_Watch_Handler(srv interface{}, stream grpc.ServerStream) error {
 	m := new(WatchRequest)
 	if err := stream.RecvMsg(m); err != nil {
 		return err
 	}
-	return srv.(APIServer).Watch(m, &aPIWatchServer{stream})
+	return srv.(PresenceAPIServer).Watch(m, &presenceAPIWatchServer{stream})
 }
 
-type API_WatchServer interface {
+type PresenceAPI_WatchServer interface {
 	Send(*WatchResponse) error
 	grpc.ServerStream
 }
 
-type aPIWatchServer struct {
+type presenceAPIWatchServer struct {
 	grpc.ServerStream
 }
 
-func (x *aPIWatchServer) Send(m *WatchResponse) error {
+func (x *presenceAPIWatchServer) Send(m *WatchResponse) error {
 	return x.ServerStream.SendMsg(m)
 }
 
-var _API_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "dogma.interop.v1.presence.API",
-	HandlerType: (*APIServer)(nil),
+var _PresenceAPI_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "dogma.interop.v1.presence.PresenceAPI",
+	HandlerType: (*PresenceAPIServer)(nil),
 	Methods:     []grpc.MethodDesc{},
 	Streams: []grpc.StreamDesc{
 		{
 			StreamName:    "Watch",
-			Handler:       _API_Watch_Handler,
+			Handler:       _PresenceAPI_Watch_Handler,
 			ServerStreams: true,
 		},
 	},
-	Metadata: "github.com/dogmatiq/interopspec/presencespec/api.proto",
+	Metadata: "github.com/dogmatiq/interopspec/presencespec/presenceapi.proto",
 }
